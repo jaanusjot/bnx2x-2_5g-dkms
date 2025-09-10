fb6e56bc74b49abb352a6dbd43fd88bab1c7f194
jaanusjot <jaanus@lap.ee>
2025-09-10 11:23:26 +0300

    Update from kernel 6.12 source
    Overwrite all modified bnx2x driver files with clean copies from 6.12 kernel source.
    https://packages.debian.org/trixie/linux-source-6.12

--- cnic_if.h
+++ cnic_if.h
@@ -13,7 +13,7 @@
 #ifndef CNIC_IF_H
 #define CNIC_IF_H
 
-#include "bnx2x_mfw_req.h"
+#include "bnx2x/bnx2x_mfw_req.h"
 
 #define CNIC_MODULE_VERSION	"2.5.22"
 #define CNIC_MODULE_RELDATE	"July 20, 2015"
@@ -190,6 +190,7 @@ struct cnic_ops {
 struct cnic_irq {
 	unsigned int	vector;
 	void		*status_blk;
+	dma_addr_t	status_blk_map;
 	u32		status_blk_num;
 	u32		status_blk_num2;
 	u32		irq_flags;
